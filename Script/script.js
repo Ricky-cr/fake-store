8fcbe65ed682d853b1167f1744e7970c238c28b5
Riccardo <ricky@MacBook-Air-di-Riccardo-2.local>
2025-01-01 19:53:35 +0100

    New features

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,18 @@
 "use strict"
 
-console.log("ciao")
+
+
+const getData = async () => {
+    let path = 'https://fakestoreapi.com/products'
+
+    try {
+        const response = await axios.get(path)
+        console.log(response)
+        
+    } catch (error) {
+        console.log("i dati non sono stati ricaricati")
+    }
+
+}
+
+getData()
